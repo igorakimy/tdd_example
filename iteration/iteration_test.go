6057ce7ea58257356ae94eb994b47f668951387c
igorakimy <igorakimy@gmail.com>
2023-03-21 17:12:05 +0300

    update iteration lesson

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -2,18 +2,31 @@ package iteration
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 const repeatCount = 5
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", repeatCount)
-	expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+	t.Run("repeat 'a' five times", func(t *testing.T) {
+		t.Helper()
+		repeated := Repeat("a", repeatCount)
+		expected := "aaaaa"
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("compare 'Repeat()' and 'strings.Repeat()'", func(t *testing.T) {
+		t.Helper()
+		customRepeat := Repeat("a", repeatCount)
+		stringsRepeat := strings.Repeat("a", repeatCount)
+		if customRepeat != stringsRepeat {
+			t.Errorf("Repeat: %q not equal strings.Repeat: %q", customRepeat, stringsRepeat)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
