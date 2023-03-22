a2f2ad635d80fb7e6460cbf72b669da5c6124a01
igorakimy <igorakimy@gmail.com>
2023-03-22 14:51:03 +0300

    update pointers and errors lesson

--- pointers_and_errors/bitcoin_test.go
+++ pointers_and_errors/bitcoin_test.go
@@ -26,4 +26,16 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err != nil {
+			t.Errorf("wanted an error but didn't get one")
+		}
+	})
 }
