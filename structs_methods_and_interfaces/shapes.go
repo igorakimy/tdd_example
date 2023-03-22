bfe4d8cfa4524fdb69b5997c41eb35caa40b50b3
igorakimy <igorakimy@gmail.com>
2023-03-22 08:16:01 +0300

    update structs, methods and interfaces lesson

--- structs_methods_and_interfaces/shapes.go
+++ structs_methods_and_interfaces/shapes.go
@@ -19,10 +19,6 @@ type Circle struct {
 	Radius float64
 }
 
-func (c Circle) Perimeter() float64 {
-	return 2 * math.Pi * c.Radius
-}
-
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
