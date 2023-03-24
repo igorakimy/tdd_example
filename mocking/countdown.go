55655ecf52d5113ac7391387b7aac9ecfea4dd37
igorakimy <igorakimy@gmail.com>
2023-03-24 10:33:26 +0300

    update mocking lesson

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -14,18 +14,18 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
 
-func (s *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
-	}
-
-	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
 
@@ -34,6 +34,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
