4d3056bbf5f3345187328abb1cd3a737662a5fac
igorakimy <igorakimy@gmail.com>
2023-03-29 12:48:29 +0300

    update 'build an application' lessons

--- app/server_test.go
+++ app/server_test.go
@@ -177,6 +177,23 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+			{"Name": "Cleo", "Wins": 10},
+			{"Name": "Chris", "Wins": 33}
+		]`)
+
+		store := FileSystemPlayerStore{database: database}
+
+		got := store.GetPlayerScore("Chris")
+
+		want := 33
+
+		if got != want {
+			t.Errorf("got %d, want %d", got, want)
+		}
+	})
 }
 
 func assertResponseBody(t *testing.T, got, want string) {
