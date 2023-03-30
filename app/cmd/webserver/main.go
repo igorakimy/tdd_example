0523877e53d60d2f2763569d136d6b151edaf74a
igorakimy <igorakimy@gmail.com>
2023-03-30 15:24:11 +0300

    update 'build an application' lessons

--- app/cmd/webserver/main.go
+++ app/cmd/webserver/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/igorakimy/poker"
 )
@@ -11,16 +10,12 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, closeFile, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("proplem opening %s, %v", dbFileName, err)
+		log.Fatal(err)
 	}
-
-	store, err := poker.NewFileSystemPlayerStore(db)
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
-	}
+	defer closeFile()
 
 	server := poker.NewPlayerServer(store)
 
